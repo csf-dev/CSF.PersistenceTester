609b178e47ef7ef150d873861919c1f523069826
Craig Fowler <craig@csf-dev.com>
2019-10-03 13:21:20 +0100

    Add documentation comments to test result

--- CSF.PersistenceTester.Core/PersistenceTestResult.cs
+++ CSF.PersistenceTester.Core/PersistenceTestResult.cs
@@ -28,18 +28,51 @@ using CSF.EqualityRules;
 
 namespace CSF.PersistenceTester
 {
+    /// <summary>
+    /// Represents the result from a persistence test.
+    /// </summary>
     public class PersistenceTestResult
     {
+        /// <summary>
+        /// Gets the <see cref="System.Type" /> of the entity type which was tested.
+        /// </summary>
         public Type TestedType { get; }
 
+        /// <summary>
+        /// Gets and sets an <see cref="System.Exception" /> which was raised during the 'setup' phase of the test (if applicable).
+        /// </summary>
         public Exception SetupException { get; set; }
 
+        /// <summary>
+        /// Gets and sets an <see cref="System.Exception" /> which was raised whilst saving the entity to the
+        /// <see cref="NHibernate.ISession" /> (if applicable).
+        /// </summary>
         public Exception SaveException { get; set; }
 
+        /// <summary>
+        /// Gets and sets an <see cref="System.Exception" /> which was raised whilst comparing the original/saved
+        /// entity with the instance retrieved from the <see cref="NHibernate.ISession" /> (if applicable).
+        /// </summary>
         public Exception ComparisonException { get; set; }
 
+        /// <summary>
+        /// Gets and sets the <see cref="EqualityResult" /> with details of the comparison between the original/saved
+        /// entity and the instance retrieved from the <see cref="NHibernate.ISession" />.  This will be <c>null</c>
+        /// if any exceptions were encountered whilst performing the test.
+        /// </summary>
         public EqualityResult EqualityResult { get; set; }
+        
+        /// <summary>
+        /// Gets a value indicating whether the original/saved entity is equal to the instance retrieved from
+        /// the <see cref="NHibernate.ISession" />.  This will also return <c>false</c> if any exceptions occurred
+        /// during the test process.
+        /// </summary>
+        public bool IsSuccess => EqualityResult?.AreEqual == true;
 
+        /// <summary>
+        /// Initialises a new instance of <see cref="PersistenceTestResult" />
+        /// </summary>
+        /// <param name="testedType">The entity type which was tested for persistence</param>
         public PersistenceTestResult(Type testedType)
         {
             TestedType = testedType ?? throw new ArgumentNullException(nameof(testedType));
