718ece42d2628778622348a78addcd060a0daed4
Craig Fowler <craig@csf-dev.com>
2019-10-24 22:12:13 +0100

    Add documentation for NUnit project
    Also fix some other docs references

--- CSF.PersistenceTester.NUnit/Constraints/TestStage.cs
+++ CSF.PersistenceTester.NUnit/Constraints/TestStage.cs
@@ -1,10 +1,24 @@
 ﻿using System;
 namespace CSF.PersistenceTester.Constraints
 {
+    /// <summary>
+    /// Enumerates the various stages of a persistence test.
+    /// </summary>
     public enum TestStage
     {
+        /// <summary>
+        /// The set-up stage.
+        /// </summary>
         Setup,
+
+        /// <summary>
+        /// The stage in which the entity is saved to the <c>ISession</c>.
+        /// </summary>
         Save,
+
+        /// <summary>
+        /// The comparison stage, where the entity is retrieved and compared with the original.
+        /// </summary>
         Compare
     }
 }
